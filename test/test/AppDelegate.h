3ba826c70bc28b84b826fd227489e3fe38759373
024581 <xiao.gengx@alibaba-inc.com>
2013-10-28 22:21:04 +0800

    first ci

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 耿 霄 on 13-10-28.
+//  Copyright (c) 2013年 耿 霄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by 耿 霄 on 13-10-28.
+//  Copyright (c) 2013年 耿 霄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
